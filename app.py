e1a18f48881af0f7cb66b0fa691577ddc0ed9248
Daniel Alm D'Adda <swedmarine@hotmail.com>
2024-02-12 13:04:03 +0100

    (untested) added function to send newsletter

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from areas.products.productPages import productBluePrint
 from areas.site.sitePages import siteBluePrint
 from dotenv import load_dotenv
 from flask import Flask
+from flask_mail import Mail
 from flask_migrate import Migrate, upgrade
 from flask_security import roles_accepted, auth_required, logout_user
 from models import db, seedData
@@ -19,6 +20,7 @@ app.config['SECURITY_PASSWORD_SALT'] = environ.get('SECURITY_PASSWORD_SALT')
 db.app = app
 db.init_app(app)
 migrate = Migrate(app,db)
+mail = Mail(app)
 # user_manager.app = app
 # user_manager.init_app(app,db,User)
 
---
+++ areas/site/services.py
@@ -0,0 +1,23 @@
+from app import mail
+from datetime import datetime
+from flask_mail import Message
+from models import Newsletter, Subscriber, db
+from sqlalchemy import select
+
+def send_newsletter(newsletter_id: int) -> None:
+    stmt = select(Newsletter).where(Newsletter.id == newsletter_id)
+    newsletter = db.session.execute(stmt).scalar()
+    if not newsletter.is_sent:
+        stmt = select(Subscriber.email).where(Subscriber.active)
+        active_subscriber_emails = db.session.execute(stmt).scalars().all()
+        with mail.connect() as conn:
+            for email in active_subscriber_emails:
+                msg = Message(recipients=[email],
+                              body=newsletter.content,
+                              subject=newsletter.subject
+                              )
+                conn.send(msg)
+        newsletter.date_sent = datetime.now()
+        newsletter.is_sent = True
+        db.session.add(newsletter)
+        db.session.commit()
