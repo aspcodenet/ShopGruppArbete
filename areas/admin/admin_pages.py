ef6c038a19f2b2a25510612d6e1949d283ed23ff
Daniel Alm D'Adda <swedmarine@hotmail.com>
2024-02-14 09:40:57 +0100

    Fixed manage_newsletters route. Added route edit_newsletter.

--- areas/admin/admin_pages.py
+++ areas/admin/admin_pages.py
@@ -1,11 +1,19 @@
 from flask import Blueprint, render_template
 
+from .admin_services import get_newsletter
+
 admin_blueprint = Blueprint('admin', __name__)
 
 @admin_blueprint.route('/admin')
 def admin() -> str:
     return render_template('admin/admin.html')
 
-admin_blueprint.route('/newsletters')
+@admin_blueprint.route('/newsletters')
 def manage_newsletters() -> str:
-    return ''
+    return render_template('admin/newsletters.html')
+
+@admin_blueprint.route('/newsletter/<newsletter_id>')
+def edit_newsletter(newsletter_id: int) -> str:
+    newsletter = get_newsletter(newsletter_id)
+    return render_template('admin/newsletter.html',
+                           newsletter = newsletter)
