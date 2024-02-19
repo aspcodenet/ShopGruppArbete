85b3adba819ccfc5f3c86de853c914074f9dac91
Daniel Alm D'Adda <swedmarine@hotmail.com>
2024-02-19 15:09:10 +0100

    Added route for sending newsletter

--- areas/admin/admin_pages.py
+++ areas/admin/admin_pages.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 
 from .admin_services import get_all_newsletter, get_newsletter
+from .admin_services import send_newsletter as sender
 
 admin_blueprint = Blueprint('admin', __name__)
 
@@ -20,3 +21,8 @@ def edit_newsletter(newsletter_id: int) -> str:
     newsletter = get_newsletter(newsletter_id)
     return render_template('admin/newsletter.html',
                            newsletter = newsletter)
+
+@admin_blueprint.route('/admin/newsletters/send/<newsletter_id>')
+def send_newsletter(newsletter_id: int) -> str:
+    sender(newsletter_id)
+    return redirect(url_for('admin.manage_newsletters'))
