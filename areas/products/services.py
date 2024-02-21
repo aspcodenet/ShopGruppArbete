30a8f6fe528d1010fe6154a46cd2e3f015fa4c74
Daniel Alm D'Adda <swedmarine@hotmail.com>
2024-02-21 11:19:10 +0100

    Added product name search functionality to search bar on all pages.

--- areas/products/services.py
+++ areas/products/services.py
@@ -1,3 +1,5 @@
+from sqlalchemy import select
+
 from models import db, Category, Product
 
 def getTrendingCategories():
@@ -61,4 +63,7 @@ def deleteProduct(id):
     if product:
         db.session.delete(product)
         db.session.commit()
-        
+
+def get_products(search_word: str) -> list[Product]:
+    stmt = select(Product).where(Product.ProductName.like(f'%{search_word}%'))
+    return db.session.execute(stmt).scalars().all()
